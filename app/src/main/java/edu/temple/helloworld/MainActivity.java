2b232ec78ae36501a343d5e285378077bf559d04
erikkapaj <eriksianobusiness@gmail.com>
2020-09-14 12:22:26 -0400

    Added a button that changed the original text view with an onClickListener.

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,12 +3,28 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button changedTextButton;
+    TextView message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        message = (TextView)findViewById(R.id.textView);
+        changedTextButton = (Button)findViewById(R.id.buttonChanged);
+
+        changedTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                message.setText("You have clicked the button!");
+            }
+        });
     }
 }
